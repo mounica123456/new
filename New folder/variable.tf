24000f23b3c6eb166e6018e774d2aab478ba6ce5
mounica123456 <mounicasathwik@outlook.com>
2020-10-08 16:32:32 +0530

    added

---
+++ New folder/variable.tf
@@ -0,0 +1,6 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
---
+++ New folder/windows-variable.tf
@@ -0,0 +1,63 @@
+provider "azurerm" {
+  version         = "=2.20.0"
+  subscription_id = "88b7a74c-47d4-48ed-b11f-9bac8f99264c"
+  client_id       = "4fe13f05-87c0-4e6e-8e33-3df885a7704e"
+  client_secret   = "4d_LfSPBooQdNN~6D8.ysrNpv3zWs-kXP0"
+  tenant_id       = "70973106-6728-471c-8ec4-08f6fa9e70bd"
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["13.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "13.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123456"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
